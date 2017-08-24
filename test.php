1b4f0697dd291f11d6ee87a004ddba8332fac00b
EC2 Default User <ec2-user@ip-172-31-36-201.us-west-2.compute.internal>
2017-08-24 16:59:33 +0000

    update test folder

--- test.php
+++ test.php
@@ -2,52 +2,57 @@
 
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
-
+date_default_timezone_set('Asia/Jerusalem');
 //initialization const
-$fileName = '~/tests.log';
+global $alert_test;
+$alert_test = false;
+$fileName = '/home/ec2-user/test/tests.log';
 $newJson = test();
-file_put_contents ($fileName ,$newJson);
+$time = date("m-d-Y H:i:s");
+$outArray = array ('DATE'=>$time,'results'=>$newJson);	
+file_put_contents ($fileName, json_encode($outArray)."\n",FILE_APPEND | LOCK_EX);
+if($alert_test)
+	sendEmail(json_encode($outArray));
+echo json_encode($outArray);
+//print_r ($newJson);
 //end of the script
 
 function test_ec2($checkName, $testCommand) {
-	$line = "";
-	$alert = false;
-	exec($testCommand, $result);
-	$line = preg_replace('/\s+/', ' ', trim($result[4]));//replace multi white space with one
-	$output = explode(" ", $line); //split the line
-	$result = "tries: $output[2], Err: $output[14] $output[15], Avg: $output[8], Min: $output[10], Max: $output[12]"
-	if(((intval($output[14]) > 0) || (intval($output[8]) > 1500))
-		$alert = true;
-	$time = date("m-d-Y H:i:s");
-	$outArray = array('DATE'=>$time,'CheckName'=>$checkName, 'Alerted'=>$alert, 'result'=>$result);		
-	return $outArray;
+        $line = "";
+        $alert = false;
+        exec($testCommand, $result);
+        $line = preg_replace('/\s+/', ' ', trim($result[4]));//replace multi white space with one
+        $output = explode(" ", $line); //split the line
+        $result = "tries: $output[2], Err: $output[14] $output[15], Avg: $output[8], Min: $output[10], Max: $output[12]";
+        if((intval($output[14]) > 0) || (intval($output[8]) > 1500))
+                $alert = true;
+        $outArray = array('CheckName'=>$checkName, 'Alerted'=>($alert)?'true':'false', 'result'=>$result);
+        return $outArray;
 }
 
 function test(){
-	$test_command = array("DNS1_1" => 'sudo /opt/jmeter/apache-jmeter-3.2/bin/jmeter -n -t /opt/jmeter/apache-jmeter-3.2/bin/NODE1_1.jmx -l /tmp/NODE1results.jtl',
-						"DNS2_1" => 'sudo /opt/jmeter/apache-jmeter-3.2/bin/jmeter -n -t /opt/jmeter/apache-jmeter-3.2/bin/NODE2_1.jmx -l /tmp/NODE2results.jtl',
-						"LOAD_BALANCER_1" => 'sudo /opt/jmeter/apache-jmeter-3.2/bin/jmeter -n -t /opt/jmeter/apache-jmeter-3.2/bin/LOAD_BALANCER_1.jmx -l /tmp/LBresults.jtl'
+        $test_command = array("DNS1_1" => 'sudo /opt/jmeter/apache-jmeter-3.2/bin/jmeter -n -t /opt/jmeter/apache-jmeter-3.2/bin/NODE1_1.jmx -l /tmp/NODE1results.jtl',
+							"DNS2_1" => 'sudo /opt/jmeter/apache-jmeter-3.2/bin/jmeter -n -t /opt/jmeter/apache-jmeter-3.2/bin/NODE2_1.jmx -l /tmp/NODE2results.jtl',
+							"LOAD_BALANCER_1" => 'sudo /opt/jmeter/apache-jmeter-3.2/bin/jmeter -n -t /opt/jmeter/apache-jmeter-3.2/bin/LOAD_BALANCER_1.jmx -l /tmp/LBresults.jtl'
 					);
-	$result_array = array();
-	$alert_test = false;
-	$i = 0;
-	foreach($test_command as $checkName=>$cmd)
-        $result_array[$i] = test_ec2($checkName, $cmd);
-		if($result_array[$i]['Alerted'] == true)
-			$alert_test = true;
-		$i++;
-    }
-	if($alert_test)
-		sendEmail($result_array);
-	return $result_array;
+        $result_array = array();        
+        $i = 0;
+        foreach($test_command as $checkName=>$cmd)   {
+			$result_array[$i] = test_ec2($checkName, $cmd);
+			if($result_array[$i]['Alerted'] == true)
+					$alert_test = true;
+			$i++;
+		}
+        return $result_array;
 }
 
 function sendEmail($result_array)    {
-	
+
 	$to="yair.shur@gmail.com";
-    $headers = 'From: testerAWS@aws.com' . "\r\n";
+	$headers = 'From: testerAWS@aws.com' . "\r\n";
 	$subject = "ALERT: Problem with NODE servers";
-	$body = $result_array[0]."\n".$result_array[1]."\n".$result_array[2]."\n";
-    mail($to, $subject, $body, $headers);
+	$body = $result_array;
+	mail($to, $subject, $body, $headers);
 }
 ?>
+
